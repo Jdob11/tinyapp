70b87626b59fd666dcb771b38cbb329db81d58e9
Jdob11 <jbdobson11@gmail.com>
2024-02-24 08:18:37 -0800

    add: Route for '/urls/:id' and corresponding template
    Added a route for '/urls/:id' in express_server.js to handle GET requests with an ID parameter. This route renders the 'urls_show.ejs' template, displaying the long URL and its corresponding short URL ID.

--- express_server.js
+++ express_server.js
@@ -22,6 +22,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:id', (req, res) => {
+  console.log(req.params);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render('urls_show', templateVars);
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
