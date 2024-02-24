435ece0701dc4b442f88926f030119b64e945804
Jdob11 <jbdobson11@gmail.com>
2024-02-24 08:31:03 -0800

    add: Comments for improved code readability
    Added comments throughout the codebase to provide clarity and context for various sections. These comments enhance readability and make it easier for developers to understand the purpose and functionality of different components.

--- express_server.js
+++ express_server.js
@@ -2,36 +2,44 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// database of short URLs and their corresponding long URLs
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+// route for the homepage
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+// route to display a simple HTML page with "Hello World"
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// route to render the 'urls_index' template with urlDatabase
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+// route to render the 'urls_show' template with specific URL information
 app.get('/urls/:id', (req, res) => {
   console.log(req.params);
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
 });
 
+// route to return the urlDatabase object as JSON
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+// start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
